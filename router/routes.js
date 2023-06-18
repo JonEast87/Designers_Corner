6d86b7fd93d13d1cc34be6781c2b5e0acc4ffd0b
JonEast87 <j.d.east@icloud.com>
2023-06-18 16:56:19 -0700

    starting back up on this app, had to pause due to swapping systems, still need some local setup before I can really continue

--- router/routes.js
+++ router/routes.js
@@ -560,4 +560,18 @@ app.delete("/jobs/:job", ensureAuthenticated, checkPoster, async (req, res) => {
 
 });
 
+
+// --- APPLYING TO JOBS //
+app.patch("/job/:job/applied", ensureAuthenticated, async (req, res) => {
+
+    try {
+        const foundJob = await Job.findOne({ jobTitle: req.params.job });
+        const foundJobID = foundJob._id;
+        const user = res.locals.currentUser;
+        foundJob.peopleApplied.push(user);
+    } catch (error) {
+        console.log(error);
+        res.status(404).send({ error: "Could not apply to this listing." });
+    }
+});
 module.exports = app;
